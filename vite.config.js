0ccfcb8cc4f60999f92e8170f641d328c0b59322
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2022-05-15 13:16:21 -0400

    Updated Dist

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-export default defineConfig({
-    base: '/portfolio/',
-    plugins: [OrbitControls()]
-})
